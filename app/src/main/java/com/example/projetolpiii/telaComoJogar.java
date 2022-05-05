dd24af01baa7e2c92c28046c6eba840a1cd49e12
ericmtfontes <ericmtfontes@gmail.com>
2022-05-05 20:57:30 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/projetolpiii/telaComoJogar.java
@@ -0,0 +1,14 @@
+package com.example.projetolpiii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaComoJogar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_como_jogar);
+    }
+}
---
+++ app/src/main/java/com/example/projetolpiii/telaSobre.java
@@ -0,0 +1,14 @@
+package com.example.projetolpiii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaSobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_sobre);
+    }
+}
